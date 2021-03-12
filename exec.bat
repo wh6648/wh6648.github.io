bf266306d799426a5169d4b5470aacf2048ca1e8
wh6648 <wh6648@hotmail.com>
2021-03-12 13:25:18 +0800

    20210312132517

--- exec.bat
+++ exec.bat
@@ -1,3 +1,10 @@
+cd D:\git\HexoAd
 call npm install
 call hexo clean
 call hexo g -d
+call hexo clean
+
+git pull
+git add .
+git commit -m "%date:~0,4%%date:~5,2%%date:~8,2%%time:~0,2%%time:~3,2%%time:~6,2%"
+git push --progress "origin" src:src
