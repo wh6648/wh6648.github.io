4614b7ac9d994eed0bb33ef06f396f90cb6fb749
wh6648 <wh6648@hotmail.com>
2021-03-24 14:01:39 +0800

    20210324140139

--- exec_ftp.bat
+++ exec_ftp.bat
@@ -10,6 +10,8 @@ git push --progress "origin" src:src
 
 rem ftp
 call D:\DCC\AD\FlashFXP\flashfxp.exe -upload ftp://sportsga:zaq12wsxCDE3@72.11.140.180:21 -localpath="D:\git\HexoAd\public\"  -remotepath="/public_html/"
+rem for /f "tokens=1" %%i in ('tasklist /nh /fi "imagename eq FlashFXP.exe"') do ( taskkill /F /PID %%i )
+rem taskkill /f /im FlashFXP.exe
 
 call hexo clean
 exit
